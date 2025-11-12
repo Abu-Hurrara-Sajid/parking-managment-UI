01498af4b45cd2f5b747b82e2d99295b52c675d0
Abu-Hurrara-Sajid <zaidmyo334@gmail.com>
2025-11-12 08:53:44 +0500

    Hello

---
+++ x.py
@@ -0,0 +1,14 @@
+
+import tkinter as tk
+
+root = tk.Tk() 
+root.geometry("300x200")  
+
+label = tk.Label(root, text="Parking Management System", font=(("Sans-Serif","bold"), 20), bg='black', fg='white') 
+label.pack() 
+
+# Label1=tk.Label1(root,text="Main Menu",font=("bold",10),)
+
+
+
+root.mainloop()  
